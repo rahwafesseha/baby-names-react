ae4083b1828e7c8e6ae8cb396aa415d2cc9f6a34
Rahwa <rahwafesseha@gmail.com>
2022-03-30 13:00:23 +0100

    level2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
+import React, {useState} from "react";
 import names from "./babyNamesData.json"
 import './App.css';
 
+function RenderNames(filteredNames) {
+  return filteredNames.names.sort((a, b) => a.name.localeCompare(b.name))
+    .map((value, index) => {
+      return (
+        <p keys={index} className={value.sex}>
+          {value.name}
+        </p>
+      );
+    });
+}
+
+function filterNames(input) { 
+  return names.filter((value) =>
+    value.name.toLowerCase().includes(input.toLowerCase())
+  );
+}
+
 function App() {
   
+  const [input, setInput] = useState("");
+  const [arrayOfNames, setArrayOfNames] = useState(names);
   return (
     <div className="App">
-      <h1>
-        Baby <span className="name">Name</span> Picker
-      </h1>
+      <h1>Baby <span className="name">Name</span> Picker</h1>
+      <input type="text"
+        onChange={(event) => {
+          setArrayOfNames(filterNames(event.target.value));
+        }}
+      ></input>{input}
       <div className="App-body">
-        {names
-          .sort((a, b) => a.name.localeCompare(b.name))
-          .map((value, index) => {
-            return (
-              <p keys={index} className={value.sex}>
-                {value.name}
-              </p>
-            );
-          })}
+       <RenderNames names={arrayOfNames} />
       </div>
     </div>
   );
